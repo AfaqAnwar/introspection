17b8a585c551561d6eaffda8ee54686a58c69811
Afaq Anwar <anwarafaq@protonmail.com>
2023-03-07 19:16:24 -0500

    Added Google Map Integration

--- lib/pages/registration/registration_tabs/basic_information/location_tab.dart
+++ lib/pages/registration/registration_tabs/basic_information/location_tab.dart
@@ -1,5 +1,11 @@
+import 'dart:async';
+
 import 'package:datingapp/data/user.dart';
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:map_picker/map_picker.dart';
 
 class LocationTab extends StatefulWidget {
   final User currentUser;
@@ -12,12 +18,70 @@ class LocationTab extends StatefulWidget {
 }
 
 class LocationTabState extends State<LocationTab> {
-  String errorMessage = "";
+  String addressStatus = "";
+  String? _currentAddress;
+  Position? _currentPosition;
+  late CameraPosition cameraPosition;
+  final _controller = Completer<GoogleMapController>();
+  MapPickerController mapPickerController = MapPickerController();
+
+  Future getLocationOnLoad() async {
+    await _getCurrentPosition();
+    cameraPosition = CameraPosition(
+      target: LatLng(_currentPosition!.latitude, _currentPosition!.longitude),
+      zoom: 14.4746,
+    );
+    if (_currentPosition != null) {
+      return true;
+    }
+  }
 
   @override
   void initState() {
     super.initState();
   }
+
+  Future<bool> _handleLocationPermission() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text(
+              'Location services are disabled. Please enable the services')));
+      return false;
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Location permissions are denied')));
+        return false;
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text(
+              'Location permissions are permanently denied, we cannot request permissions.')));
+      return false;
+    }
+    return true;
+  }
+
+  Future<void> _getCurrentPosition() async {
+    final hasPermission = await _handleLocationPermission();
+    if (!hasPermission) return;
+    await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
+        .then((Position position) {
+      setState(() => _currentPosition = position);
+    }).catchError((e) {
+      debugPrint(e);
+    });
+  }
+
+  String errorMessage = "";
 
   void updateLocationOfUser() {}
 
@@ -27,28 +91,97 @@ class LocationTabState extends State<LocationTab> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: const [
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 25.0),
-              child: Text(
-                "Where do you live?",
-                textAlign: TextAlign.left,
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 36,
-                  fontFamily: 'Marlide-Display',
-                  fontWeight: FontWeight.w800,
+    return FutureBuilder(
+      future: getLocationOnLoad(),
+      builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+        if (snapshot.data == true) {
+          return Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: const [
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 25.0),
+                    child: Text(
+                      "Where do you live?",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 36,
+                        fontFamily: 'Marlide-Display',
+                        fontWeight: FontWeight.w800,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 25),
+              SizedBox(
+                width: MediaQuery.of(context).size.width - 50,
+                height: 300,
+                child: MapPicker(
+                  // pass icon widget
+                  iconWidget: const Icon(Icons.location_on,
+                      color: Colors.red, size: 40),
+                  //add map picker controller
+                  mapPickerController: mapPickerController,
+                  child: GoogleMap(
+                    myLocationEnabled: true,
+                    zoomControlsEnabled: false,
+                    // hide location button
+                    myLocationButtonEnabled: false,
+                    mapType: MapType.normal,
+                    //  camera position
+                    initialCameraPosition: cameraPosition,
+                    onMapCreated: (GoogleMapController controller) {
+                      _controller.complete(controller);
+                    },
+                    onCameraMoveStarted: () {
+                      // notify map is moving
+                      mapPickerController.mapMoving!();
+                      addressStatus = "checking ...";
+                    },
+                    onCameraMove: (cameraPosition) {
+                      this.cameraPosition = cameraPosition;
+                    },
+                    onCameraIdle: () async {
+                      // notify map stopped moving
+                      mapPickerController.mapFinishedMoving!();
+                      //get address name from camera position
+
+                      List<Placemark> placemarks =
+                          await placemarkFromCoordinates(
+                        cameraPosition.target.latitude,
+                        cameraPosition.target.longitude,
+                      );
+
+                      // update the ui with the address
+                      addressStatus =
+                          '${placemarks.first.name}, ${placemarks.first.administrativeArea}, ${placemarks.first.country}';
+                    },
+                  ),
                 ),
               ),
-            ),
-          ],
-        ),
-        const SizedBox(height: 25),
-      ],
+              const SizedBox(height: 25),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Text(
+                  addressStatus,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontFamily: 'Modern-Era',
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ],
+          );
+        } else {
+          return const Center(child: CircularProgressIndicator());
+        }
+      },
     );
   }
 }
