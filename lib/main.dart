f7ea2f02af1669fc923cd54e8b88f8b152328e26
Afaq Anwar <anwarafaq@protonmail.com>
2023-03-29 22:52:49 -0400

    Added Model Support

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:datingapp/auth/auth_page.dart';
+import 'package:datingapp/pages/personaility_pages/personailty_chat_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/services.dart';
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: const AuthPage(),
+        home: const PersonailtyChatPage(),
         theme: ThemeData(fontFamily: 'Modern-Era'));
   }
 }
--- lib/pages/personaility_pages/personailty_prediction_result_page.dart
+++ lib/pages/personaility_pages/personailty_prediction_result_page.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:sklite/naivebayes/naive_bayes.dart';
+import 'package:sklite/utils/io.dart';
 
 class PersonailtyPredictionResultPage extends StatefulWidget {
   const PersonailtyPredictionResultPage(
@@ -12,6 +15,15 @@ class PersonailtyPredictionResultPage extends StatefulWidget {
 
 class _PersonailtyPredictionResultPage
     extends State<PersonailtyPredictionResultPage> {
+  late GaussianNB nb;
+  @override
+  void initState() {
+    super.initState();
+    loadModel("assets/data/models/nm_ei.json").then((x) {
+      nb = GaussianNB.fromMap(jsonDecode(x));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
