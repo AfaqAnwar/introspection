6822a2d1f766ab9ead549d0c9360a823f3e55078
Afaq Anwar <anwarafaq@protonmail.com>
2023-04-20 04:27:59 -0400

    Update home_page_host.dart

--- lib/pages/home_page_host.dart
+++ lib/pages/home_page_host.dart
@@ -27,7 +27,6 @@ class _HomePageHostState extends State<HomePageHost>
     _pageController = PageController(initialPage: 1);
     potentialMatches = [];
     matches = [];
-    getMatches();
     super.initState();
   }
 
